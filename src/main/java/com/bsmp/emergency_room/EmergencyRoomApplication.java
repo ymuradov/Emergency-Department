1f667faecca132a457c105ede8186c00337f18a1
yusuf.muradov <yusuf.muradov@physiq.com>
2021-11-22 18:25:12 +0300

    init commit

---
+++ src/main/java/com/bsmp/emergency_room/EmergencyRoomApplication.java
@@ -0,0 +1,13 @@
+package com.bsmp.emergency_room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmergencyRoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bsmp/emergency_room/EmergencyRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsmp.emergency_room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
